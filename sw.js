18735aeaeef546443505d17272b82eb9617399f7
killianadnot <killian.adnot@gmail.com>
2020-11-23 11:56:04 +0100

    ajout de sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,28 @@
 var cacheName = "ginko-v1";
+var contentToCache = [];
+var icons=["32","64","96","128","168","180","192","256","512"];
 
-var images = ["icon-32", "icon-64", "icon-96", "icon-128", "icon-168", "icon-180", "icon-192", "icon-256", "icon-512"];
-
-var contentCache = [];
-
-for(var i=0; i<images.length; i++) {
-  contentCache.push('/icons/'+images[i]+'.png');
+for(var i=0; i<icons.length; i++) {
+    contentToCache.push('/icons/icon-'+icons[i]+'.png');
 }
 
-contentCache.push('index.html');
+contentToCache.push('index.html');
+contentToCache.push('app.js');
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
-      caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all: app shell and content');
-        return cache.addAll(contentCache);
-      })
-    );
+        caches.open(cacheName).then((cache) => {
+              console.log('[Service Worker] Caching all: app shell and content');
+          return cache.addAll(contentToCache);
+        })
+      );
 });
 
-/*self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-            return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-                    console.log('[Service Worker] Caching new resource: '+e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
-});*/
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-      caches.open('mysite-dynamic').then(function (cache) {
+      caches.open('ginko-dynamic').then(function (cache) {
         return cache.match(event.request).then(function (response) {
           return (
             response ||
