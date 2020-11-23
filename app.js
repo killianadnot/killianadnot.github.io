2da8903ecbf04adfbc9803ff5352342da8dca7c2
killianadnot <killian.adnot@gmail.com>
2020-11-23 11:34:33 +0100

    ajout de sw.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js');
-}
+};
 
 /************************************************************************
  *                                                                      *
